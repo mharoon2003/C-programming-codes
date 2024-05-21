fa7050329b2be8482b764b945502b13fc745b5bd
M.Haroon <mharoonn2003@gmail.com>
2024-05-21 11:05:41 +0430

    the code is changed

--- first - Copy.c
+++ first - Copy.c
@@ -1,9 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> /*this is pre processor command that tells the compiler to add the content of which file from library of C */
 
-int main(){
+int main(){               
+     /*this is the main methode or main function of the program that compiler starts compiling from this oint of our code and this is necessery for every program that we want to compile that */
 
-printf("This is my first code from C programming language");
+printf("This is my first code from C programming language"); 
+ /* ; this sign is used to end every statement in C programming language*/
 
 return 0;
+/*(this signs are // and /* for comments that are ignored by compiler while compiling)   */
 
 }
+
