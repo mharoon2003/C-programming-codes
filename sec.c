3302a84d2e0648013c0f3235ffeaef5422a9039c
M.Haroon <mharoonn2003@gmail.com>
2024-05-21 11:39:50 +0430

    This is the example of using escape sequences

--- sec.c
+++ sec.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/*escape sequences : charcter combination of a backslash ( / ) followed by letter or combinition of digits 
+they specify action withen a line or digit or text
+/n = goes to new line
+  /t = creates a certain distance between characters or digits   */
 
 int main(){
 
-printf("This is my first code from C programming language");
+printf("This is my first code from C programming language \n" );
+
+printf("1\t2\t3\t4\n5");// here 5 goes to next line in output
+printf("\" I love You\" he said ");  // when we want print or use "" in output we might use them us (\" The  text here  \")
 
 return 0;
 
